512b40597c2713fe4cb3ac626523037b1f151db1
Grigoriy Shishmintsev <chesminsky@MacBook-Pro-Grigoriy.local>
2019-09-21 14:04:27 +0300

    add inputs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MDCTextField } from '@material/textfield';
 
 @Component({
   selector: 'esb-login',
@@ -10,6 +11,7 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
   }
 
 }
