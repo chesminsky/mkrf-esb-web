1a74f26bc56f1af3e11f2a7c705688b12911440b
chesminsky <chesminsky@gmail.com>
2019-09-22 01:07:22 +0300

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { AuthGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [{
   path: 'login',
   component: LoginComponent
 }, {
   path: '',
-  component: MainComponent
+  component: MainComponent,
+  canActivate: [ AuthGuard ],
+}, {
+  path: '**',
+  redirectTo: ''
 }];
 
 @NgModule({
