b4e5e10c4a9681e7e56ec3feae0f1aab92475a90
Grigoriy Shishmintsev <gvshishm@mts.ru>
2019-09-25 15:31:15 +0300

    api url

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private apiUrl = environment.apiUrl;
 
   constructor(
     private http: HttpClient
@@ -15,7 +18,7 @@ export class AuthService {
     const headers = new HttpHeaders({
       Authorization: 'Basic ' + btoa(`${username}:${password}`)
     });
-    return this.http.get('/esb/adm/login', { headers })
+    return this.http.get(this.apiUrl + '/esb/adm/login', { headers })
       .pipe(tap((resp: any) => {
         if (resp.token) {
           localStorage.setItem('auth', resp.token);
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
+  private apiUrl = environment.apiUrl;
+
   constructor(
       private http: HttpClient
   ) { }
 
   getUsers() {
-      return this.http.get('esb/adm/users');
+      return this.http.get(this.apiUrl + '/esb/adm/users');
   }
 }
